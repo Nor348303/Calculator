c65984eea44bd50fad2a5232f2c88411251c0058
Calculator <2019348303@student.uitm.edu.my>
2021-06-12 10:05:05 +0800

    Calculator

--- app/src/main/java/com/example/calculator/Profile.java
+++ app/src/main/java/com/example/calculator/Profile.java
@@ -2,12 +2,15 @@ package com.example.calculator;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
 import android.widget.Button;
 import android.content.Intent;
 import android.view.View;
+import android.widget.TextView;
 
 public class Profile extends AppCompatActivity {
     public Button button;
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +18,10 @@ public class Profile extends AppCompatActivity {
         setContentView(R.layout.activity_profile);
 
         button = (Button) findViewById(R.id.btn);
+        textView = findViewById(R.id.textView5);
+        textView.setMovementMethod (LinkMovementMethod.getInstance());
+
+
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
